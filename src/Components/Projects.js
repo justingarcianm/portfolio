e4222ffcc2556198aa58014c41dff0c9aba7ad37
justin garcia <justinframedev@gmail.com>
2019-03-20 15:44:19 -0600

    start project text and upload images

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,9 +3,19 @@ import React from 'react';
 
 function Projects() {
 	return (
-		<div>
-			<h1>Projects</h1>
-		</div>
+		<section>
+			<div className="row">
+				<div className="col s12">
+					<h2>Projects</h2>
+				</div>
+				<div className="col s12 m6">
+
+				</div>
+				<div className="col s12 m6">
+
+				</div>
+			</div>
+		</section>
 	)
 }
 export default Projects;
