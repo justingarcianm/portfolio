afd5fc4d6e8f8bc4e90c97763c74d53bcf3ff729
justin garcia <justinframedev@gmail.com>
2019-03-22 00:46:00 -0600

    remove react media query package

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -30,19 +30,19 @@ function Header() {
 	}
 
 	return (
-		<header>
-			<section className="container" id="home" onMouseOver={navToggle}>
+		<header onMouseOver={navToggle}>
+			<section className="container" id="home" >
 				<h1>Hi, I'm Justin</h1>
 
 				<p className="accent">A web developer</p>
 
-				<div className="show-arrow">
-					<i className="material-icons" onClick={togglehiddenText}>expand_more</i>
-				</div>
+
 				<div className="hidden-text">
 					<p>Since 2018 I have commited myself to teaching myself web development. I am front end focused using React, but I also have the skills to take on full stack with Node. Below are some of my projects and ways to get a hold of me.</p>
 				</div>
-
+				<div className="show-arrow">
+					<i className="material-icons" onClick={togglehiddenText}>expand_more</i>
+				</div>
 			</section>
 		</header>
 	)
