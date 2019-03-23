fbad8ea60ee99079e79f9ec1e4343ea65edd85fc
justin garcia <justinframedev@gmail.com>
2019-03-22 23:05:12 -0600

    Updates

---
+++ precache-manifest.677357d434608aa30f33d11e4f312702.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d6a2d9532f8d911b3c1259748eb527c",
+    "url": "/portfolio/static/media/web_resume_indica_labs.3d6a2d95.pdf"
+  },
+  {
+    "revision": "dd981f64026bafcf60fe65fa74df03a5",
+    "url": "/portfolio/static/media/localBistro.dd981f64.JPG"
+  },
+  {
+    "revision": "83828d77b236ec41b456ce34a50d26a6",
+    "url": "/portfolio/static/media/blogSite.83828d77.JPG"
+  },
+  {
+    "revision": "61734e785057678de51b20c3d892b892",
+    "url": "/portfolio/static/media/artistSite.61734e78.JPG"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "cca49c8207488b4d2a9c",
+    "url": "/portfolio/static/js/main.b4196686.chunk.js"
+  },
+  {
+    "revision": "4231adcdc479f41c33cb",
+    "url": "/portfolio/static/js/2.4947ba8c.chunk.js"
+  },
+  {
+    "revision": "cca49c8207488b4d2a9c",
+    "url": "/portfolio/static/css/main.d4f8bfbc.chunk.css"
+  },
+  {
+    "revision": "58fe29856459947bb7db3a3504ada2ff",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/justingarcianm/portfolio/precache-manifest.7ea5e025b4105060130019856bfb2598.js"
+  "/portfolio/precache-manifest.677357d434608aa30f33d11e4f312702.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/justingarcianm/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
