0124cf6430038385e6761a2094b16e1336cc6b29
justin garcia <justinframedev@gmail.com>
2019-03-22 22:55:40 -0600

    Updates

---
+++ precache-manifest.7ea5e025b4105060130019856bfb2598.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d6a2d9532f8d911b3c1259748eb527c",
+    "url": "/justingarcianm/portfolio/static/media/web_resume_indica_labs.3d6a2d95.pdf"
+  },
+  {
+    "revision": "dd981f64026bafcf60fe65fa74df03a5",
+    "url": "/justingarcianm/portfolio/static/media/localBistro.dd981f64.JPG"
+  },
+  {
+    "revision": "83828d77b236ec41b456ce34a50d26a6",
+    "url": "/justingarcianm/portfolio/static/media/blogSite.83828d77.JPG"
+  },
+  {
+    "revision": "61734e785057678de51b20c3d892b892",
+    "url": "/justingarcianm/portfolio/static/media/artistSite.61734e78.JPG"
+  },
+  {
+    "revision": "f17ee9eaacf8e736add3",
+    "url": "/justingarcianm/portfolio/static/js/runtime~main.78c7dd68.js"
+  },
+  {
+    "revision": "cca49c8207488b4d2a9c",
+    "url": "/justingarcianm/portfolio/static/js/main.b4196686.chunk.js"
+  },
+  {
+    "revision": "4231adcdc479f41c33cb",
+    "url": "/justingarcianm/portfolio/static/js/2.4947ba8c.chunk.js"
+  },
+  {
+    "revision": "cca49c8207488b4d2a9c",
+    "url": "/justingarcianm/portfolio/static/css/main.d4f8bfbc.chunk.css"
+  },
+  {
+    "revision": "4d09ccdf8343f8cd80ae573d13c6e68d",
+    "url": "/justingarcianm/portfolio/index.html"
+  }
+];
